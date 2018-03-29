a11037cd57c2c689a0f9733e71323326d56e0288
bcaplins <37717532+bcaplins@users.noreply.github.com>
2018-03-28 21:20:03 -0600

    Added imshiftError function

---
+++ imageError.m
@@ -0,0 +1,34 @@
+function err = imageError(A,B,shift)
+    
+    l1 = [1 1];
+    l2 = l1+shift;
+    r1 = size(A);
+    r2 = size(B)+shift;
+    
+    leftX   = max( l1(1), l2(1) );
+    rightX  = min( l1(1) + size(A,1), l2(1) + size(B,1) );
+    topY    = max( l1(2), l2(2) );
+    bottomY = min( l1(2) + size(A,2), l2(2) + size(B,2) );
+
+    if ( leftX < rightX && topY < bottomY ) 
+      Ais = leftX:(rightX-1);
+      Ajs = topY:(bottomY-1);
+      
+      Bis = Ais-shift(1);
+      Bjs = Ajs-shift(2);
+      
+%       err = sum(sum((A(Ais,Ajs)-B(Bis,Bjs)).^2))./(length(Ais)*length(Ajs));
+      err = sum(sum((A(Ais,Ajs).*B(Bis,Bjs)).^2))./(length(Ais)*length(Ajs));
+      
+      
+      
+    else
+      err = NaN;
+      return
+    end
+
+
+end
+    
+
+    
--- startup.m
+++ startup.m
@@ -1,10 +1,15 @@
-disp('HELLLLLLO!!!') 
+disp(' ')
+disp('Climb the mountains and get their good tidings.')
+disp('Natures peace will flow into you as sunshine flows into trees.') 
+disp('The winds will blow their own freshness into you,') 
+disp('and the storms their energy,') 
+disp('while cares will drop off like autumn leaves')
+disp('    - John Muir')
+disp(' ')
 
-
-addpath C:\Users\bwc\Documents\MATLAB\customfunctions\
-addpath C:\Users\bwc\Documents\MATLAB\NIST_dsc_data\
-addpath C:\Users\bwc\Documents\MATLAB\MyClasses\
-addpath C:\Users\bwc\Documents\MATLAB\MyFunctions\
+addpath NIST_dsc_data\
+addpath MyClasses\
+addpath MyFunctions\
 
 set(0,'defaultlinelinewidth',2)
 set(0,'DefaultAxesFontSize', 10)
